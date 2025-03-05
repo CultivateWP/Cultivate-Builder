87166591b8ca0add36f3caaaba7e6025de8a967a
Bill Erickson <bill@cultivatewp.com>
2025-03-05 10:40:32 -0600

    add lodash dependency

--- functions.php
+++ functions.php
@@ -63,7 +63,7 @@ add_action( 'wp_enqueue_scripts', 'cwp_scripts' );
  * Gutenberg scripts and styles
  */
 function cwp_gutenberg_scripts() {
-	wp_enqueue_script( 'theme-editor', get_theme_file_uri( '/assets/js/editor.js' ), array( 'wp-blocks', 'wp-dom' ), filemtime( get_theme_file_path( '/assets/js/editor.js' ) ), true );
+	wp_enqueue_script( 'theme-editor', get_theme_file_uri( '/assets/js/editor.js' ), array( 'wp-blocks', 'wp-dom', 'lodash' ), filemtime( get_theme_file_path( '/assets/js/editor.js' ) ), true );
 }
 add_action( 'enqueue_block_editor_assets', 'cwp_gutenberg_scripts' );
 
